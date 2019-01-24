136f1d0fefd66fe366f2521c4e54e83eafb82460
Kang SuSeong <maxkss0829@naver.com>
2019-01-24 17:56:30 +0900

    Add files via upload

---
+++ env.js
@@ -0,0 +1,19 @@
+(function(){
+  window.primEnv = window.primEnv || {
+    API_ROOT: "https://api.shinycolors.enza.fun/",
+
+    ASSET_ROOT: "/assets/",
+
+    TRACKING_URI: "https://tracking.shinycolors.enza.fun/",
+
+    ENABLE_CRYPTO: false,
+
+    PF_URI: "https://platform.enza.fun/",
+
+    GAME_ID: 01,
+
+    X_VERSION: 133,
+
+    STATS: false
+  };
+}());
